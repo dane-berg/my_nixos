3464bb6af58233cc6526988aac6ec00b32e67cfe
Dane <dayneberg12@gmail.com>
2025-02-27 22:39:10 -0500

    include cached changes in rebuild-nixos.sh commit

--- rebuild-nixos.sh
+++ rebuild-nixos.sh
@@ -22,7 +22,7 @@ else
 fi
 
 # Early return if no changes were detected (thanks @singiamtel!)
-if git diff --quiet; then
+if git diff --quiet && git diff --cached --quiet; then
     echo "No changes detected, exiting."
     popd
     exit 0
@@ -32,8 +32,9 @@ fi
 alejandra . &>/dev/null \
   || ( alejandra . ; echo "formatting failed!" && exit 1)
 
-# Shows your changes
-git diff -U0
+# Adds and shows your changes
+git add .
+git diff --cached -U0
 
 echo "NixOS Rebuilding..."
 
@@ -44,7 +45,7 @@ sudo nixos-rebuild switch --flake /etc/nixos#default &>nixos-switch.log || (cat 
 current=$(nixos-rebuild list-generations | grep current)
 
 # Commit all changes witih the generation metadata
-git commit -am "$current"
+git commit -m "$current"
 
 # Back to where you were
 popd
