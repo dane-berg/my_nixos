5c035cd775844c082dd31ae0cea314b935fda9ad
dane-berg <dayneberg12@gmail.com>
2025-05-01 18:24:18 -0400

    disable autosuspend
    38 current  2025-05-01 18:30:46  25.05.20250424.f771eb4     6.12.24                          *

--- modules/core.nix
+++ modules/core.nix
@@ -6,6 +6,13 @@
     nixup = "/etc/nixos/update-nixos.sh";
   };
 
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
